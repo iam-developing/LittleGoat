e2d5e11011f64adae02d013a34938fe8c820f44f
Marzell Michalski <mars030211@gmail.com>
2020-07-26 11:26:30 +0200

    Added activity game.

--- app/src/main/java/com/kleineerdbeere/littlegoat/MainActivity.java
+++ app/src/main/java/com/kleineerdbeere/littlegoat/MainActivity.java
@@ -2,13 +2,34 @@ package com.kleineerdbeere.littlegoat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button gameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gameButton = (Button) findViewById(R.id.btn_game);
+        gameButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                openGameActivity();
+            }
+        });
+
+
+    }
+
+    public void openGameActivity() {
+        Intent intent = new Intent(this, ActivityGame.class);
+        startActivity(intent);
     }
 }
